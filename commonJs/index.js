fad7f93340adc9d86b6e7c7f3a0d0861f8e38c4b
Sindre <sinfr91@gmail.com>
2022-06-11 21:43:58 +1200

    success

--- commonJs/index.js
+++ commonJs/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash'
 
 const app = document.getElementById('App')
-const arr = []
+const arr = [4, 4, 4, 4, 4, 4]
 _.fill(arr, 6)
-app.innerHTML = arr.map((el) => '<p>el</p>')
+arr.map((el) => {
+  const p = document.createElement('p')
+  p.innerHTML = el
+  app.appendChild(p)
+})
+console.log(app)
+console.log(arr)
