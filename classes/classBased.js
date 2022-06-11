638cedf322d5fa661ba202e358054cec1b3d4360
Sindre <sinfr91@gmail.com>
2022-06-11 21:08:15 +1200

    refactor to class constructor

--- classes/classBased.js
+++ classes/classBased.js
@@ -1,18 +1,19 @@
 let myLibrary = []
 
-function Book(title, author, pages, read = false) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = () =>
+class Book {
+  constructor(title, author, pages, read = false) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+  info = () =>
     console.log(
       `${this.title} by ${this.author}, ${this.pages} pages, ${
         this.read ? 'read' : 'not read yet'
       }.`
     )
 }
-
 const myDiary = new Book('diary', 'me', 6, true)
 
 function addBookToLibrary(book) {
@@ -21,5 +22,5 @@ function addBookToLibrary(book) {
 
 addBookToLibrary(myDiary)
 
-console.log(myLibrary)
+console.log(myDiary.prototype)
 myDiary.info()
